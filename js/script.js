0c3cb8fcd9072e23581b6fa27c0c0a1f03fb73b5
Beth1998 <tatyliz1998@gmail.com>
2024-12-04 12:52:32 -0500

    animacionbtnHamburg

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-// Obtenemos el botón hamburguesa y el menú
-    const hamburger = document.getElementById('hamburger');
+
+
+    // Selecciona el botón de hamburguesa y el menú
+    const hamburger = document.querySelector('.hamburger');
     const menu = document.getElementById('menu');
-    
-    // Añadimos un evento de clic al botón hamburguesa
-    hamburger.addEventListener('click', function() {
-        // Alternamos la clase 'show' que maneja la visibilidad del menú
+
+    // Cuando se hace clic en el botón hamburguesa
+    hamburger.addEventListener('click', () => {
+        // Alterna la clase "open" para animar las barras
+        hamburger.classList.toggle('open');
+        // Alterna la visibilidad del menú
         menu.classList.toggle('show');
     });
